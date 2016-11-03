2557df1368b14d98384453fb4715e410681b3c7b
david-borchers <dlb@st-andrews.ac.uk>
2016-11-03 09:11:26 +0000

    added tost_sl analysis and some other things

--- Tost-nonEuc.r
+++ Tost-nonEuc.r
@@ -1,36 +1,50 @@
 library(secr)
 library(fields)
-<<<<<<< HEAD
-=======
+library(maptools)
 source("scrplotting.r")
 
 
->>>>>>> e21a5349b49949344f30dc2353bfce5cd48ea799
 #Running SECR for Tost 2012
+
+# Read capture file and boundary
 all.data.Tost<-read.capthist(captfile = "./Tost/Tost_capthist2012.csv", trapfile = "./Tost/Tost_cams_rugged2012.csv", detector="count", fmt = "trapID", trapcovnames = c("Effort",	"Topo",	"Altidute",	"Rgd"))
 boundaryTost=readShapeSpatial("./Tost//Habitat/TostStudy_Area.shp")
+# and plot it
 plot(boundaryTost)
 plot(x=all.data.Tost, add=TRUE)
+
+# Make mask:
 TostMask=make.mask(traps(all.data.Tost), spacing=500, buffer = 25000, type="trapbuffer", poly=boundaryTost)
+
+# Read ruggedness covariate and put it into mask covariate GRIDCODE
 SLCost.Tost<-readShapePoly("./Tost//Habitat/Tost_Rgd500m.shp")  #ruggedness pixels averaged over 500m radius
-
-head(SLCost.Tost)
-head(TostMask)
-
 TostMask1<-addCovariates(TostMask, SLCost.Tost)
 
-# Standarize Rgd (this makes fits a bit more stable)
-# --------------------------------------------------
+# Read binary habitat suitability code into mask covariate ...
+SLCostBINARY.Tost<-readShapePoly("./Tost//Habitat/tost_sl.shp")  #ruggedness pixels averaged over 500m radius
+TostMask1<-addCovariates(TostMask1, SLCostBINARY.Tost)
+names(covariates(TostMask1))[3:4] = c("binaryID","BINCODE")
+summary(covariates(TostMask1))
+# make NAs in BINCODE zeros:
+covariates(TostMask1)$BINCODE[is.na(covariates(TostMask1)$BINCODE)] = 0
+summary(covariates(TostMask1))
+
+
+# Standarize Rgd on traps (this makes fits a bit more stable)
+# -----------------------------------------------------------
 summary(covariates(traps(all.data.Tost)))
 covariates(traps(all.data.Tost))$stdRgd = scale(covariates(traps(all.data.Tost))$Rgd)
 summary(covariates(traps(all.data.Tost)))
 head(covariates(traps(all.data.Tost)))
 
-# Also standarize stdGRIDCODE for good measure
-# -----------------------------------------
+# Standarize GRIDCODE (in stdGC) and BINCODE (in stdBC) on mask
+# ------------------------------------------------------------------------
 summary(covariates(TostMask1))
 covariates(TostMask1)$stdGC = scale(covariates(TostMask1)$GRIDCODE)
+covariates(TostMask1)$stdBC = scale(covariates(TostMask1)$BINCODE)
+summary(covariates(TostMask1))
 names(covariates(TostMask1))
+
 
 head(covariates(TostMask1))
 summary(covariates(TostMask1))
@@ -84,9 +98,10 @@ userdfn2 <- function (xy1, xy2, mask) {
   costDistance(trans, as.matrix(xy1), as.matrix(xy2))
 }
 
+# Model with stdGC in noneuc:
+# ---------------------------
 Tost.hhn.DHab.nonU<-secr.fit(all.data.Tost, detectfn="HHN", mask=TostMask1,
-                             model=list(D~stdGC, lambda0~1, sigma~1, 
-                                        noneuc ~ stdGC -1), 
+                             model=list(D~stdGC, lambda0~1, sigma~1, noneuc ~ stdGC -1), 
                              details = list(userdist = userdfn1),
                              start = list(noneuc = 1))
 coefficients(Tost.hhn.DHab.nonU)
@@ -116,7 +131,21 @@ plot(Tost.cams,add=TRUE)
 Nhat1D1.nonU<-region.N(Tost.hhn.D.nonU)
 Nhat1D1.nonU
 
+# Model with stdGC and stdBC in noneuc:
+# -------------------------------------
+Tost.hhn.DHab.nonU.GB<-secr.fit(all.data.Tost, detectfn="HHN", mask=TostMask1,
+                             model=list(D~stdGC, lambda0~1, sigma~1, noneuc ~ stdGC + stdBC-1), 
+                             details = list(userdist = userdfn1),
+                             start = list(noneuc = 1))
+coefficients(Tost.hhn.DHab.nonU.GB)
+TostSurface.nonU<-predictDsurface(Tost.hhn.DHab.nonU.GB, se.D=TRUE, cl.D=TRUE)
+plot(TostSurface.nonU.GB,asp=1,contour=FALSE,col=terrain.colors(40))
+plot(Tost.cams,add=TRUE)
+plotcovariate(TostSurface.nonU.GB,covariate="stdGC",asp=1,contour=FALSE)
+plot(Tost.cams,add=TRUE)
 
+Nhat1.nonU<-region.N(Tost.hhn.DHab.nonU.GB)
+Nhat1.nonU
 
 # Compare with and without non-Euclidian distance:
 # -----------------------------------------------
@@ -134,21 +163,45 @@ AIC(Tost.hhn.D.nonU,Tost.hhn.DHab.nonU,Tost.hhn.DHab)
 
 # get density range so plot on same scale
 Dlim=range(covariates(TostSurface.nonU)$D.0,covariates(TostSurface)$D.0)
+
 # Do plots next to each other:
-windows() #Opens a separate window for plotting maps
 par(mfrow=c(2,1))
 plot.Dsurface(TostSurface,asp=1,contour=FALSE,col=terrain.colors(40),zlim=Dlim)
-<<<<<<< HEAD
-plot.Dsurface(TostSurface.nonU,asp=1,contour=FALSE,col=terrain.colors(40),zlim=Dlim)
-=======
 plot(TostSurface.nonU,asp=1,contour=FALSE,col=terrain.colors(40),zlim=Dlim)
->>>>>>> e21a5349b49949344f30dc2353bfce5cd48ea799
 # Hmmm, that is pretty unclear; try on log scale:
 Dhat=TostSurface; covariates(Dhat)$D.0=log(covariates(Dhat)$D.0)
 Dhat.nonU=TostSurface.nonU; covariates(Dhat.nonU)$D.0=log(covariates(Dhat.nonU)$D.0)
 Dlim=range(covariates(Dhat.nonU)$D.0,covariates(Dhat)$D.0)
 plot(Dhat,asp=1,contour=FALSE,col=terrain.colors(40),zlim=Dlim)
 plot(Dhat.nonU,asp=1,contour=FALSE,col=terrain.colors(40),zlim=Dlim)
+
+# Plot non-euclidian distance map, selecting origin with cursor.
+dmap <- function (traps, mask, userd, i = 1, ...) {
+  if (is.na(i)) i <- nearesttrap(unlist(locator(1)), traps) 
+  covariates(mask) <- data.frame(d = userd[i,]) 
+  covariates(mask)$d[!is.finite(covariates(mask)$d)] <- NA 
+  plotcovariate(mask, covariate = 'd',  ...)
+  points(traps[i,], pch = 3, col = 'red')
+}
+# plot gridcode
+quartz(h=5,w=10)
+plotcovariate(TostSurface.D.nonU,covariate="stdGC",asp=1,contour=FALSE,col=terrain.colors(40))
+text(Tost.cams,labels=as.character(1:40),cex=0.75)
+# plot distance from given trap
+trap=7 # choose trap
+dmask=TostMask1
+covariates(dmask)$noneuc = log(covariates(TostMask1)$noneuc +1) # log for better plot
+im=prep4image(data.frame(x=dmask$x,y=dmask$y,z=covariates(dmask)$stdGC),plot=FALSE)
+quartz(h=5,w=10)
+crfun=function(x) sqrt(prod(x))
+userd = nedist(Tost.cams,dmask,dmask,transitionFunction=crfun)
+dmap(Tost.cams, dmask, userd, i=trap, contour=FALSE)
+dmap(Tost.cams, dmask, userd, i=NA, contour=FALSE)
+points(Tost.cams[trap,],pch=19,cex=2,col="white")
+points(Tost.cams[trap,],pch=19,cex=1.5)
+contour(im,add=TRUE,col="gray",lwd=0.5,nlevels=6)
+text(Tost.cams,labels=as.character(1:40),cex=0.75,col="white")
+
 
 # Plot measure of probability of detection by given trap, from all points on mask:
 lambda0=exp(coef(Tost.hhn.DHab.nonU)["lambda",1]) # on the real scale
