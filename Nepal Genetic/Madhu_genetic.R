0d582b1561253caf31d531e149c7f937f5564067
koustubhsharma <koustubhsharma@gmail.com>
2017-11-08 16:06:26 +0600

    Genetic data

--- Nepal Genetic/Madhu_genetic.R
+++ Nepal Genetic/Madhu_genetic.R
@@ -5,7 +5,7 @@ source("scrplotting.r")
 library(gdistance)
 
 Trial.Data1<-read.capthist(captfile = "./Nepal Genetic/Madhu_Caps2.txt",
-                          trapfile ="./Nepal Genetic/Madhu_T2.txt", detector="transect",fmt = "XY")
+                           trapfile ="./Nepal Genetic/Madhu_T2.txt", detector="transect",fmt = "XY")
 
 Madhu.Data1<-read.capthist(captfile = "./Nepal Genetic/Madhu_Caps_Fin.txt", binary.usage=FALSE,
                            trapfile ="./Nepal Genetic/Madhu_Traps_Fin_NoCov.txt", detector="transect",
@@ -16,18 +16,29 @@ Madhu.Data2<-read.capthist(captfile = "./Nepal Genetic/Madhu_Caps_Fin.txt", bina
                            trapcovnames = c("Topography","Habitats","Altitude"))
 
 boundaryMadhu=readShapeSpatial("C:/Users/Koustubh/Dropbox (Snow Leopard Trust)/CREEM/Nepal/Madhu_poly1.shp")
+boundaryMadhu2=readShapeSpatial("C:/Users/Koustubh/Dropbox (Snow Leopard Trust)/CREEM/Nepal/Madhu_poly_clip.shp")
 
 plot(boundaryMadhu)
+plot(boundaryMadhu2)
+plot(traps(Madhu.Data1), add=TRUE)
 plot(Madhu.Data1, add=TRUE)
+traps(Madhu.Data2)
 
 # Make mask:
-MadhuMask=make.mask(traps(Madhu.Data1), spacing=1000, buffer = 12500, type="trapbuffer", 
+MadhuMask=make.mask(traps(Madhu.Data1), spacing=1000, type="traprect", buffer = 30000, 
                     poly=boundaryMadhu)
-plot(MadhuMask)
-RPSV(Madhu.Data1,CC=TRUE)
+MadhuMask2=make.mask(traps(Madhu.Data2), spacing=1000, buffer = 35000, type="traprect", 
+                     poly=boundaryMadhu2) #Creates a rectangular buffer around detectors, and clips to polygon
+#boundaryMadhu2 excludes habitat below and above SL range
 
-Madhu.hhn<-secr.fit(Madhu.Data1, model=list(D~1, lambda0~1, sigma~1), detectfn="HHN", mask=MadhuMask)
-region.N()
+?make.mask
+
+plot(MadhuMask2)
+RPSV(Madhu.Data2,CC=TRUE)
+summary(Madhu.Data2)
+
+Madhu.hhn<-secr.fit(Madhu.Data2, model=list(D~1, lambda0~1, sigma~1), detectfn="HHN", mask=MadhuMask)
+region.N(Madhu.hhn)
 
 summary(Madhu.Data2)
 # Standarize Rgd on traps (this makes fits a bit more stable)
@@ -37,12 +48,30 @@ covariates(traps(Madhu.Data2))$stdAlt = scale(covariates(traps(Madhu.Data2))$Alt
 summary(covariates(traps(Madhu.Data2)))
 head(covariates(traps(Madhu.Data2)))
 
-summary(Trial.Data1)
+# Read ruggedness covariate and put it into mask covariate GRIDCODE
+SLRgd.Madhu<-readShapePoly("C:/Users/Koustubh/Dropbox (Snow Leopard Trust)/CREEM/Nepal/Rugged_1000shp.shp")  #ruggedness pixels averaged over 500m radius
+SLAlt.Madhu<-readShapePoly("C:/Users/Koustubh/Dropbox (Snow Leopard Trust)/CREEM/Nepal/Altitude.shp")
+# using focal statistics tool
+
+MadhuMask2a<-addCovariates(MadhuMask2, SLRgd.Madhu)
+MadhuMask2a<-addCovariates(MadhuMask2a, SLAlt.Madhu)
+names(covariates(MadhuMask2a))[3:4] = c("Rgd","Alt")
+
+summary(MadhuMask2a)
+plotcovariate(MadhuMask2a, covariate = "GRIDCODE", asp =1, contour = FALSE)
 
 
+Madhu.hhn.lam_Topo<-secr.fit(Madhu.Data2, model=list(D~1, lambda0~"Topography", sigma~1), 
+                             detectfn="HHN", mask=MadhuMask)
 
+Madhu.hhn.lam_Alt<-secr.fit(Madhu.Data2, model=list(D~1, lambda0~"stdAlt", sigma~1), 
+                            detectfn="HHN", mask=MadhuMask)
 
+Madhu.hhn.lam_Habitat<-secr.fit(Madhu.Data2, model=list(D~1, lambda0~"Topography", sigma~1), 
+                                detectfn="HHN", mask=MadhuMask)
 
+skMadhu.hhn.D_alt<-secr.fit(Madhu.Data2, model=list(D~"StdAltitude", lambda0~"Topography", sigma~1), 
+                            detectfn="HHN", mask=MadhuMask)
 
 
 all.data.Zoloon<-read.capthist(captfile = "./Nepal Genetic/Zoloon_caps.csv", 
@@ -57,6 +86,6 @@ summary(all.data.Zoloon)
 
 plot(ZoloonMask)
 Zoloon.hhn<-secr.fit(all.data.Zoloon, model=list(D~1, lambda0~1, sigma~1), 
-                         detectfn="HHN", mask=ZoloonMask)
+                     detectfn="HHN", mask=ZoloonMask)
 derived(Zoloon.hhn)
 region.N(Zoloon.hhn)
