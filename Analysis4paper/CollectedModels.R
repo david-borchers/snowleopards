0c946b74611bfb1cd89c83c29b1900cbbbae6cda
david-borchers <dlb@st-andrews.ac.uk>
2019-08-07 19:27:07 +0100

    added Realised and Expected N and D tables

--- Analysis4paper/CollectedModels.R
+++ Analysis4paper/CollectedModels.R
@@ -16,6 +16,7 @@ AIC(TNN.GCmean_dev.WW.session.sigma,
 #  Old models created by Koustubh (*_Thinkpad.RData) and David (*_all.RData) (some of these use lambda0, not a0)
 load("./Analysis4paper/TNNnoNEfits_Thinkpad.RData")
 load("./Analysis4paper/TNNnoNEfits_all.RData")
+load("./Analysis4paper/TNNnoNEfits.RData")
 TNN_AICc<-AIC(TNNfit.DGridXSess.a0Sess.sigsess, TNNfit.DGrid_Sess.a0Sess.sigsess, TNNfit.DGrid_Sess.a0TopoXSess.sigsess, TNNfit.DGrid_Sess.a0Topo.Sess.sigsess, 
               TNNfit.DGrid.a0Topo.sig1, TNNfit.DGrid.a0TopoSess.sig1, TNNfit.DGrid.a0TopoSess.sigsess, TNNfit.DGridSess.lamSess.SigSess, TNN.DGrid.LamTopo.Sig1, 
               TNN.DGridXsess.LamTopoXsess, TNN.DGridXsess.DetTopoXsess, TNNfit.D1.LamSess.sig1, TNNfit.D1.Lam1s.sig1, TNNfit.Dsess.Lamsess.sigsess, 
@@ -87,6 +88,11 @@ fit2 = TNNfit.DGrid.lam0Waterxsess_topo.sig0
 vcv2 = fit2$beta.vcv
 mvnmean2 = fit2$fit$par
 Nhat2 = region.N(fit2)
+# survey areas:
+A = rep(NA,3)
+for(i in 1:3) A[i] = summary(fit$mask[[i]])$cellarea*dim(fit$mask[[i]])[1] / 100  # area in sq km
+# Sample sizes
+n = c(Nhat1[[1]]$n[1],Nhat1[[2]]$n[1],Nhat1[[3]]$n[1])
 
 B = 1000 # number resamples
 b.par1 = rmvnorm(n=B, mean=mvnmean1, sigma=vcv1) # get the resampled parameter estimates
@@ -116,14 +122,54 @@ for(i in 1:B) {
 close(pb) # close progress bar
 
 save(b.RN1,b.RN2,b.EN1,b.EN2,file="./Analysis4paper/TNNbest2fitsBootN.RData")
+load("./Analysis4paper/TNNbest2fitsBootN.RData")
 
-n = c(Nhat1[[1]]$n[1],Nhat1[[2]]$n[1],Nhat1[[3]]$n[1])
-b.mean1 = apply(b.RN1,2,mean)
-b.mean2 = apply(b.RN2,2,mean)
-b.se1 = apply(b.RN1,2,sd)
-b.se2 = apply(b.RN2,2,sd)
+# Estimates with Realised N
+# -------------------------
+b.meanRN1 = apply(b.RN1,2,mean)
+b.meanRN2 = apply(b.RN2,2,mean)
+b.seRN1 = apply(b.RN1,2,sd)
+b.seRN2 = apply(b.RN2,2,sd)
+b.ciRN1 = apply(b.RN1,2,quantile,probs=c(0.025,0.975))
+b.ciRN2 = apply(b.RN2,2,quantile,probs=c(0.025,0.975))
 n
-b.mean1;b.mean2
-b.mean2/b.mean1
-b.se1;b.se2
+b.meanRN1;b.meanRN2
+b.meanRN2/b.meanRN1
+RN1 = c(Nhat1[[1]][2,1],Nhat1[[2]][2,1],Nhat1[[3]][2,1])
+RN2 = c(Nhat2[[1]][2,1],Nhat2[[2]][2,1],Nhat2[[3]][2,1])
+b.meanRN1/RN1 # bstrap mean/original est of RN
+b.seRN1;b.seRN2
+RNtable = data.frame(N1 = RN1, se.N1=b.seRN1, pcv.N1=100*b.seRN1/RN1, lcl.N1=b.ciRN1[1,], ucl.N1=b.ciRN1[2,],
+                     N2 = RN2, se.N2=b.seRN2, pcv.N2=100*b.seRN2/RN2, lcl.N2=b.ciRN2[1,], ucl.N2=b.ciRN2[2,])
+round(RNtable,1)
+RDtable = RNtable
+for(i in 1:3) {
+  RDtable[i,-c(3,8)] = RDtable[i,-c(3,8)]/A[i] * 100 # density in 100 sq km
+}
+signif(RDtable,4)
+
+
+# Estimates with Expected N
+# -------------------------
+b.meanEN1 = apply(b.EN1,2,mean)
+b.meanEN2 = apply(b.EN2,2,mean)
+b.seEN1 = apply(b.EN1,2,sd)
+b.seEN2 = apply(b.EN2,2,sd)
+b.ciEN1 = apply(b.EN1,2,quantile,probs=c(0.025,0.975))
+b.ciEN2 = apply(b.EN2,2,quantile,probs=c(0.025,0.975))
+n
+b.meanEN1;b.meanEN2
+b.meanEN2/b.meanEN1
+EN1 = c(Nhat1[[1]][2,1],Nhat1[[2]][2,1],Nhat1[[3]][2,1])
+EN2 = c(Nhat2[[1]][2,1],Nhat2[[2]][2,1],Nhat2[[3]][2,1])
+b.meanEN1/EN1 # bstrap mean/original est of EN
+b.seEN1;b.seEN2
+ENtable = data.frame(N1 = EN1, se.N1=b.seEN1, pcv.N1=100*b.seEN1/EN1, lcl.N1=b.ciEN1[1,], ucl.N1=b.ciEN1[2,],
+                     N2 = EN2, se.N2=b.seEN2, pcv.N2=100*b.seEN2/EN2, lcl.N2=b.ciEN2[1,], ucl.N2=b.ciEN2[2,])
+round(ENtable,1)
+EDtable = ENtable
+for(i in 1:3) {
+  EDtable[i,-c(3,8)] = EDtable[i,-c(3,8)]/A[i] * 100 # density in 100 sq km
+}
+signif(EDtable,4)
 
